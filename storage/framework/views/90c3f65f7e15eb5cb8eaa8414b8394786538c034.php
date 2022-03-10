29b54aefe15b2d26fbf7c0b5aa747d2f37d400c3
Bamba Touré <96212892+bamba297@users.noreply.github.com>
2022-03-10 23:44:16 +0000

    Add files via upload

---
+++ storage/framework/views/90c3f65f7e15eb5cb8eaa8414b8394786538c034.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>paiement des chambres</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="<?php echo e(asset('css/accueil.css')); ?>">
+    </head>
+    <body>
+        <nav>
+            <button class="deconnexion">Déconnexion</button>
+        </nav>
+        <header>
+            <div class="onglets">
+				<a style="border-bottom: 5px solid red;"  href="index.html">Accueil</a>
+				<a href="Voiture.html">Gestion des paiements</a>
+				<a href="commande.html">Gestion des quitus</a>
+                <a href="commande.html">Alerte</a>
+                <a href="commande.html">Aide</a>
+			</div>
+        </header>
+        <section class="main" id="voiture">
+            <div class="content">
+                <div class="card">
+                    <div class="left">
+                        <h3>LOGEMENT CROUS</h3>
+                        <p>
+                            <label>LOGEMENT EN RESIDENCE UNIVERSITAIRE</label>
+                           <ul>
+                               <li>Nos résidences universitaires</li>
+                               <li>Demande de logement en résidence CROUS</li>
+                               <li>Paiement du loyer en ligne</li>
+                               <li>Désistements et préavis de départ d'un logement</li>
+                           </ul>
+                        </p>
+                    </div>
+                    <section class="main1">
+            <div id="carouse">
+                <div class="content1">
+                    <img class="image" src="img/village1.webp">
+                    <img class="image" src="img/village2.webp">
+                    <img class="image" src="img/village3.jpg">
+                    <img class="image" src="img/village4.jpg">
+                    <img class="image" src="img/village5.webp">
+                </div>
+            </div>
+        </section>
+        <footer>
+		<p>
+			<h1 class="services">CROUS UGB</h1>
+			<label class="pp">Email : crous.ugb@ugb.edu.sn</label>
+			<label class="service">Contact: +221 77 655 14 00 | &copy; 2022, CrousUgb</label>
+		</p>
+	</footer>
+    </body>
+</html><?php /**PATH C:\wamp64\e-chambres\resources\views/accueil.blade.php ENDPATH**/ ?>
